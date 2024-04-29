1ade6650a3b936cf00a53bd5d06b59ffb7284ce4
Valejandro08 <143618679+Valejandro08@users.noreply.github.com>
2024-04-29 14:40:09 +0000

    new

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 // Declare your arrays here
-
+let image= document.querySelector("#image");
+let mood= ["Anticipation","Fear","Adventurous","Silly"];
+let Anticipation=["missing.jpg","Get Out.jpg","Now.jpg"];
+let Fear= ["sinister.jpg","Conjuring.jpg ","insidious.jpg "];
+let Adventurous=["Maze Runner.jpg","Potter.jpg ","lod.jpg"];
+let Silly= ["KungFu.jpg","TheHeat.jpg","The_Man_From_Toronto.jpg"];
 // Make sure to declare your HTML elements as variables! 
 
 // Submit Button 
 let submitButton = document.querySelector("button");
-submitButton.addEventListener("click", function() {});
+submitButton.addEventListener("click", function() {
+    if (mood=== 
+});
