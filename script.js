ba5d18c4d52bcc5aaa7b1cb4a7d5ba01587f6538
Valejandro08 <143618679+Valejandro08@users.noreply.github.com>
2024-05-02 14:40:55 +0000

    new

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 // Declare your arrays here
-let image= document.querySelector("#image");
-let mood= ["Anticipation","Fear","Adventurous","Silly"];
-let Anticipation=["missing.jpg","Get Out.jpg","Now.jpg"];
-let Fear= ["sinister.jpg","Conjuring.jpg ","insidious.jpg "];
-let Adventurous=["Maze Runner.jpg","Potter.jpg ","lod.jpg"];
-let Silly= ["KungFu.jpg","TheHeat.jpg","The_Man_From_Toronto.jpg"];
+let anticipationUrls=["missing.jpg","Get Out.jpg","Now.jpg"];
+let fearUrls= ["sinister.jpg","Conjuring.jpg ","insidious.jpg "];
+let adventurousUrls=["Maze Runner.jpg","Potter.jpg ","lod.jpg"];
+let sillyUrls= ["KungFu.jpg","TheHeat.jpg","The_Man_From_Toronto.jpg"];
+
 // Make sure to declare your HTML elements as variables! 
+
+let input= document.querySelector("input").value;
 
 // Submit Button 
 let submitButton = document.querySelector("button");
+let mood;
+    
 submitButton.addEventListener("click", function() {
-    if (mood=== 
+   
+    if (input === "Anticipation") {
+ mood= anticipationUrls;
+    }
+   else if(input==="Fear"){
+    mood=fearUrls;
+   }
+    else if (input=== "Adventurous"){
+      mood=adventurousUrls;
+    }
+       else if (input==="Silly"){
+        mood=sillyUrls;
+       }
+  
+});
+input.forEach(function(url){
+let image=document.createElement("img");
+image.src=url;
+mood.appendChild(image);
+
 });
